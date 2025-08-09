7eda9da6920732db738da600589143d9c5d58199
kdhdd <kdso10@naver.com>
2025-08-09 22:36:19 +0900

    :recycle: Router 주석 제거, env 파일 제거

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,12 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import OrderMethod from "../pages/OrderMethod";
 // 추후 추가될 페이지들을 위한 임포트 (현재는 주석 처리)
-// import Chat from "./pages/Chat";
-// import About from "./pages/About";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import Profile from "./pages/Profile";
-// import Settings from "./pages/Settings";
 // import NotFound from "./pages/NotFound";
 
 export default function AppRouter() {
@@ -18,13 +12,6 @@ export default function AppRouter() {
         {/* 메인 홈페이지 */}
         <Route path="/" element={<Home />} />
         <Route path="/order-method" element={<OrderMethod />}/>
-        {/* 추후 추가될 라우트들 */}
-        {/* <Route path="/chat" element={<Chat />} /> */}
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/register" element={<Register />} /> */}
-        {/* <Route path="/profile" element={<Profile />} /> */}
-        {/* <Route path="/settings" element={<Settings />} /> */}
 
         {/* 404 페이지 (모든 라우트의 마지막에 위치) */}
         {/* <Route path="*" element={<NotFound />} /> */}
